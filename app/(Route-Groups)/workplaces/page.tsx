2eb6d50fa027fe634d0fcf16c8b2b4b217e1f316
safwansaf123 <safwan_saf@ymail.com>
2024-12-01 01:21:48 +0500

    first commit

---
+++ app/(Route-Groups)/workplaces/page.tsx
@@ -0,0 +1,14 @@
+
+import Routegroups from "../page";
+export default function Workplace(){
+
+    return(
+
+        <div>
+            
+        <h4>Aviation Maintenace Technician</h4>
+        <h3>Student at Governor IT initaitive</h3>
+        <Routegroups/>
+        </div>
+    )
+};
